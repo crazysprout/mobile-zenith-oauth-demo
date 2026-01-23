e53bcf0c66f4c1e2fde57b06cfa07a0c92f76626
Nutty Macbook <>
2026-01-24 02:55:37 +0700

    Rename zenith-release.aar to zenith.aar and update build script

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,13 +51,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-    // Zenith SDK (Local AAR)
-    implementation(files("libs/zenith-release.aar"))
-
-    // Required SDK transitive dependencies
-    implementation(libs.moshi)
-    implementation(libs.okhttp)
+}
+   implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.moshi)
