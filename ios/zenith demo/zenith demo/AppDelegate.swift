a8e763b3e8cf880c361bab3b8ef2fc6aef49b635
Nutty Macbook <>
2026-02-05 20:28:49 +0700

    chore: clear sensitive API keys and deeplink configuration

--- ios/zenith demo/zenith demo/AppDelegate.swift
+++ ios/zenith demo/zenith demo/AppDelegate.swift
@@ -11,12 +11,7 @@ import Zenith
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        // Staging
-        ZenithApp.shared.setup(apiKey: "4bfa58a20f559c88a80ba6db4727e98b8c52f99aa501a0dbd8268c520d1bb373")
-        
-        // Production
-        // ZenithApp.shared.setup(apiKey: "e67d72bdc76ebfe2b30ff30c16cb3ab1bef81243ca4fc344ee6ccedaa077d5c9")
+         ZenithApp.shared.setup(apiKey: "YOUR_API_KEY")
         return true
     }
 
@@ -33,7 +28,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
